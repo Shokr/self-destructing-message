d31457690daf26ac3ccf00078ff019bbfc6a1b66
Mohammed Shokr <mohammedshokr2014@gmail.com>
2016-03-23 12:48:03 +0200

    config

---
+++ config/app.php
@@ -0,0 +1,17 @@
+<?php
+
+return [
+
+    'url' => 'http://localhost/',
+
+    'db' => [
+
+        'mysql' => [
+            'host' => '127.0.0.1',
+            'dbname' => 'destructy',
+            'username' => 'root',
+            'password' => 'root',
+        ],
+
+    ],
+];
